0cc537b6b4b5c0bfdd6bed1f120e8854d088c88b
unknown <JLIO0558@DIGITALCOLLEGE.LOCAL>
2024-10-10 20:07:00 -0300

    Adicionando lógica para adiantar e retornar as páginas

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ import characterWrite from "./components/characterWrite.js";
 import { nextPage, previousPage } from "./components/pages.js";
 
 let bodyText = document.getElementById('body-text');
+let header = document.getElementById('header');
 let url = 'https://rickandmortyapi.com/api/character';
 
-const renderizar = (url) => {fetch(url).then(function(response){
+window.renderizar = function(url){fetch(url).then(function(response){
     return response.json();
 }).then(function(responseBody){
     bodyText.innerHTML = '';
@@ -13,6 +14,12 @@ const renderizar = (url) => {fetch(url).then(function(response){
 
     const arrayText = info.map((character) => characterWrite(character))
     bodyText.innerHTML += arrayText.join('');
+
+    header.innerHTML = `
+            <img onclick="renderizar('${pages.prev}')" class="seta" src="./icones/seta-esquerda.png">
+            <h1>Rickpedia</h1>
+            <img onclick="renderizar('${pages.next}')" class="seta" src="./icones/seta-direita.png">
+    `
 })
 }
 
